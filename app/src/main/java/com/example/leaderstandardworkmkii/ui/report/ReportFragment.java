62d0a9ef8dab0033dbdddc9afb88036a99715418
anton <antoniodeca88@gamil.com>
2021-07-15 20:01:24 -0500

    Export CSV method added to ReportFragment Still no working

--- app/src/main/java/com/example/leaderstandardworkmkii/ui/report/ReportFragment.java
+++ app/src/main/java/com/example/leaderstandardworkmkii/ui/report/ReportFragment.java
@@ -1,5 +1,8 @@
 package com.example.leaderstandardworkmkii.ui.report;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,12 +12,16 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 
 import com.example.leaderstandardworkmkii.databinding.FragmentReportBinding;
+
+import java.io.File;
+import java.io.FileOutputStream;
 
 public class ReportFragment extends Fragment {
 
@@ -37,8 +44,43 @@ public class ReportFragment extends Fragment {
                 textView.setText(s);
             }
         });
+
+
         return root;
     }
+    public void export(View view){
+        //generate data
+        StringBuilder data = new StringBuilder();
+        data.append("Time,Distance");
+        for(int i = 0; i<5; i++){
+            data.append("/n"+ (i) + ","+ (i*i));
+        }
+        try{
+            //saving the file into device
+            FileOutputStream out = getContext().openFileOutput("data.csv", Context.MODE_PRIVATE);
+            out.write((data.toString()).getBytes());
+            out.close();
+
+            //exporting
+            Context context = getActivity().getApplicationContext();
+            File filelocation =  new File(getActivity().getFilesDir(), "data.csv");
+            Uri path = FileProvider.getUriForFile(context, "com.example.LeaderStandardWorkMKII.fileprovider", filelocation);
+            Intent fileintent = new Intent(Intent.ACTION_SEND);
+            fileintent.setType("text/csv");
+            fileintent.putExtra(Intent.EXTRA_SUBJECT, "Data");
+            fileintent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            fileintent.putExtra(Intent.EXTRA_STREAM, path);
+            startActivity(Intent.createChooser(fileintent, "Send mail"));
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+
+    }
+
 
     @Override
     public void onDestroyView() {
